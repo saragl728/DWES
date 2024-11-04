e1ea3c536f62ad3318d7fd1a8ba31d1d61824421
saragl728 <saragl728@gmail.com>
2024-11-04 12:24:42 +0100

    Añado el ejercicio a1_11

---
+++ Bloque A/a1_11.php
@@ -0,0 +1,28 @@
+<?php
+$item = 'Chocolate';
+$stock = 5;
+$wanted = 8;
+$deliver = true
+$canBuy = $(($stock <= $wanted) && ($deliver == true));
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<title>Multidimensional Arrays</title>
+<link rel="stylesheet" href="css/styles.css">
+</head>
+<body>
+<h1>The Candy Store</h1>
+<h2>Shopping Cart</h2>
+<?php
+    //intercambio los valores
+    $aux = $stock
+    $stock = $wanted
+    $wanted = $aux
+?>
+<p>Items: <?= $items ?></p>
+<p>Stock: <?=$stock ?></p>
+<p>Wanted: <?=$wanted ?></p>
+<p>Can buy: <??$canBuy ?></p>
+</body>
+</html>
