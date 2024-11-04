a12229c3bfdab8aeb66eac409086ce154ba4c639
saragl728 <saragl728@gmail.com>
2024-11-04 11:50:04 +0100

    Añado el ejercicio a1_9

---
+++ Bloque A/a1_9.php
@@ -0,0 +1,18 @@
+<?php
+$prefix = 'Thank you';
+$name
+= 'Sara';
+$message = "$prefix $name";
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<title>String Operator</title>
+<link rel="stylesheet" href="css/styles.css">
+</head>
+<body>
+<h1>The Candy Store</h1>
+<h2><?= $name ?>'s Order</h2>
+<p><?= $message ?></p>
+</body>
+</html>
